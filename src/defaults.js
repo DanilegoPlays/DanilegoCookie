a5b238117721f04db6fdcb0f4cab7513331b60ab
DanilegoPlays <dcarvalhogarcia@gmail.com>
2026-08-07 01:35:32 -0300

    Versão 9.0 - Alquimia
    Versão 9.0 - Laboratório de Alquimia
    - Novo minigame adicionado
    - 16 combinações especiais de poções para serem descobertas, e muito mais combinações genéricas com vários efeitos
    - UI dos minigames melhoradas
    - novo ícone para as torres
    - novas conquistas (incluindo secretas!)

--- src/defaults.js
+++ src/defaults.js
@@ -19,7 +19,7 @@ import { velocityPerSecond } from 'framer-motion';
 
 // Estruturas padrão para construções e melhorias
 export const DEFAULT_CONSTRUCOES = [
-    {nome: "Vovó", preço: 15, cps: 1111110.5, quantidade: 0, quantidadeGratis: 0, descricao: "Uma vovó para assar cookies fresquinhos do forno", icone: Vovo1, icone_pequeno: Vovo3},
+    {nome: "Vovó", preço: 15, cps: 0.5, quantidade: 0, quantidadeGratis: 0, descricao: "Uma vovó para assar cookies fresquinhos do forno", icone: Vovo1, icone_pequeno: Vovo3},
     {nome: "Fazenda", preço: 100, cps: 1.5, quantidade: 0, descricao: "As Fazendas plantam e colhem Pés de Cookie", icone: Fazenda, icone_pequeno: Fazenda},
     {nome: "Mina", preço: 1000, cps: 8, quantidade: 0, descricao: "As Minas mineram Cookie Mineral direto da terra!", icone: Mina, icone_pequeno: Mina},
     {nome: "Fábrica", preço: 11000, cps: 45, quantidade: 0, descricao: "As Fábricas produzem cookies em larga escala", icone: Fabrica, icone_pequeno: Fabrica},
